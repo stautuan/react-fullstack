edefa08f6f1245daf457b724e8b2f8e8400e9e58
stautuan <stautuan89@gmail.com>
2024-05-30 12:35:08 -0400

    Changed text color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import SearchBar from "./SearchBar";
 
 function Navbar() {
   return (
-    <nav className="border-b-2 border-stone-300 bg-stone-200">
+    <nav className="border-b-2 border-neutral-300 bg-neutral-200">
       <div className="container mx-auto flex items-center justify-between  px-2.5 py-4">
         <div>
-          <h1 className="text-2xl font-medium text-stone-500">Bookstore.</h1>
+          <h1 className="text-2xl font-medium text-neutral-500">Bookstore.</h1>
         </div>
         <SearchBar />
-        <ul className="-mr-2 flex items-center gap-x-5 font-medium text-stone-400">
+        <ul className="-mr-2 flex items-center gap-x-5 font-medium text-neutral-400 text-neutral-500">
           <li className="px-2 py-1">Books</li>
           <li className="px-2 py-1">About</li>
           <li className="px-2 py-1">
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,14 @@ import { MagnifyingGlass } from "@phosphor-icons/react";
 
 function SearchBar() {
   return (
-    <div className="flex rounded-full border-2 border-stone-300">
+    <div className="flex rounded-full border-2 border-neutral-300">
       <input
         type="text"
         placeholder="Search books, author, genre..."
         className=" w-60 rounded-l-full px-4 py-1 placeholder:text-sm  focus:outline-sky-500"
       />
       <button
-        className="cursor-pointer rounded-r-full bg-white px-3 py-1 text-stone-400"
+        className="cursor-pointer rounded-r-full bg-white px-3 py-1 text-neutral-400"
         aria-label="Search"
       >
         <MagnifyingGlass size={20} weight="bold" />
