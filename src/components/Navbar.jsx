a3a720ec7924591d6b16456f8ad4887e91a97bb8
stautuan <stautuan89@gmail.com>
2024-05-31 15:40:23 -0400

    Removed a div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,11 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 border-b-2 border-neutral-300 bg-neutral-200">
+    <nav className="sticky top-0 border-b-2 border-neutral-300 bg-neutral-200 shadow">
       <div className="container mx-auto flex flex-wrap items-center justify-between p-4 sm:flex-nowrap">
-        <div>
-          <h1 className="text-2xl font-medium text-neutral-500">
-            <Link to="/">bookstore.</Link>
-          </h1>
-        </div>
+        <h1 className="text-2xl font-medium text-neutral-500">
+          <Link to="/">bookstore.</Link>
+        </h1>
         <SearchBar />
         <ul className="-mr-2 hidden font-medium text-neutral-500 lg:flex lg:items-center lg:gap-x-5">
           <li className="px-2 py-1">
