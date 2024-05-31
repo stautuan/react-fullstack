1d758954d1f21b45353b71bb244c5d4a514f61f2
stautuan <stautuan89@gmail.com>
2024-05-30 20:08:03 -0400

    Fixed bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section>
-      <div className="bg-hero flex h-[40rem] w-screen flex-col items-center justify-center divide-y divide-neutral-900 bg-cover bg-left-bottom bg-no-repeat">
+      <div className="flex h-[40rem] w-full flex-col items-center justify-center divide-y divide-neutral-900 bg-hero bg-cover bg-left-bottom bg-no-repeat">
         <h2 className="text-4xl font-bold">Dear Reader</h2>
         <p className="text-xl">From My Library to Yours</p>
       </div>
