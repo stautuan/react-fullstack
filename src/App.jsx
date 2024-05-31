777a0abc8dec22eb4132e19e7a5bc1dfee3967fe
stautuan <stautuan89@gmail.com>
2024-05-31 09:34:20 -0400

    Implemented routing for page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Home from "./pages/Home";
+import AppLayout from "./components/AppLayout";
+import BooksPage from "./pages/BooksPage";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/books",
+        element: <BooksPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-      {/* TODO: */}
-      {/* router */}
-      {/* create app layout */}
-      {/* create books data */}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import { Bag, List } from "@phosphor-icons/react";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="border-b-2 border-neutral-300 bg-neutral-200">
       <div className="container mx-auto flex flex-wrap items-center justify-between p-4 sm:flex-nowrap">
         <div>
-          <h1 className="text-2xl font-medium text-neutral-500">bookstore.</h1>
+          <h1 className="text-2xl font-medium text-neutral-500">
+            <Link to="/">bookstore.</Link>
+          </h1>
         </div>
         <SearchBar />
         <ul className="-mr-2 hidden font-medium text-neutral-500 lg:flex lg:items-center lg:gap-x-5">
-          <li className="px-2 py-1">Books</li>
-          <li className="px-2 py-1">About</li>
+          <li className="px-2 py-1">
+            <Link to="/books">Books</Link>
+          </li>
+          <li className="px-2 py-1">
+            <Link to="/about">About</Link>
+          </li>
           <li className="px-2 py-1">
             <Bag size={26} />
           </li>
