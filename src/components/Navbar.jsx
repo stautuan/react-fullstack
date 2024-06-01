098dd95fab2b21a8e08bcaa6922e48cfce50a47e
stautuan <stautuan89@gmail.com>
2024-06-01 15:20:56 -0400

    Added state management for responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Bag, List } from "@phosphor-icons/react";
 import SearchBar from "./SearchBar";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
   return (
     <nav className="sticky top-0 border-b-2 border-neutral-300 bg-neutral-200 shadow">
       <div className="container mx-auto flex flex-wrap items-center justify-between p-4 sm:flex-nowrap">
@@ -10,6 +13,13 @@ function Navbar() {
           <Link to="/">bookstore.</Link>
         </h1>
         <SearchBar />
+        <button
+          className="lg:hidden"
+          type="button"
+          onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+        >
+          <List size={26} weight="bold" />
+        </button>
         <ul className="-mr-2 hidden font-medium text-neutral-500 lg:flex lg:items-center lg:gap-x-5">
           <li className="px-2 py-1">
             <Link to="/books">Books</Link>
@@ -21,9 +31,6 @@ function Navbar() {
             <Bag size={26} />
           </li>
         </ul>
-        <button className="lg:hidden" type="button">
-          <List size={26} weight="bold" />
-        </button>
       </div>
     </nav>
   );
