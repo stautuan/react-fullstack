18ae8eba06ef1b93441000e363498baed1e05951
stautuan <stautuan89@gmail.com>
2024-06-07 11:23:56 -0400

    Refactored list section in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,24 @@ function Navbar() {
     setShowLinks(!showLinks);
   }
 
+  const links = [
+    {
+      id: 1,
+      url: "/books",
+      text: "Books",
+    },
+    {
+      id: 2,
+      url: "/about",
+      text: "About",
+    },
+    {
+      id: 3,
+      url: "/",
+      icon: <Bag size={26} />,
+    },
+  ];
+
   return (
     <nav className="border-b-2 border-neutral-300 bg-neutral-200 shadow">
       <div className="container mx-auto">
@@ -26,15 +44,17 @@ function Navbar() {
         {showLinks && (
           <div>
             <ul className="px-4 py-2 font-medium text-neutral-500">
-              <li className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3">
-                <Link to="/books">Books</Link>
-              </li>
-              <li className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3">
-                <Link to="/about">About</Link>
-              </li>
-              <li className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3">
-                <Bag size={26} />
-              </li>
+              {links.map((link) => {
+                const { id, url, text, icon } = link;
+                return (
+                  <li
+                    key={id}
+                    className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3"
+                  >
+                    <Link to={url}>{text || icon}</Link>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         )}
