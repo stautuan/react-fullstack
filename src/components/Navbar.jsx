f86c1bbb35bf8db1e3325f030327b450c9a0f530
stautuan <stautuan89@gmail.com>
2024-06-07 13:34:38 -0400

    Implemented a responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
 import SearchBar from "./SearchBar";
 import { Bag, List } from "@phosphor-icons/react";
 
@@ -37,30 +36,40 @@ function Navbar() {
             <Link to="/">.bookstore</Link>
           </h1>
           <SearchBar />
-          <button
-            className="transition-all hover:rotate-90"
-            onClick={toggleLinks}
-          >
+          <ul className="hidden px-4 py-2 font-medium text-neutral-500 lg:flex lg:items-center">
+            {links.map((link) => {
+              const { id, url, text, icon } = link;
+              return (
+                <li
+                  key={id}
+                  className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3"
+                >
+                  <Link to={url}>{text || icon}</Link>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="lg:hidden" onClick={toggleLinks}>
             <List size={32} />
           </button>
         </div>
-        {showLinks && (
-          <div>
+        <div>
+          {showLinks && (
             <ul className="px-4 py-2 font-medium text-neutral-500">
               {links.map((link) => {
                 const { id, url, text, icon } = link;
                 return (
                   <li
                     key={id}
-                    className="px-2 py-1 transition-all hover:rounded hover:bg-neutral-300 hover:px-3"
+                    className="px-2 py-1 hover:rounded hover:bg-neutral-300 hover:px-3"
                   >
                     <Link to={url}>{text || icon}</Link>
                   </li>
                 );
               })}
             </ul>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </nav>
   );
