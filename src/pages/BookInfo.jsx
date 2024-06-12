879b96fdbd0d6c1a9a75f36e6aab52a237bd7de2
stautuan <stautuan89@gmail.com>
2024-06-12 08:29:10 -0400

    Added redux toolkit

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { formatPrice } from "../utils";
 
 function BookInfo() {
   const { id } = useParams();
@@ -24,40 +25,60 @@ function BookInfo() {
     <section className="h-screen">
       {book ? (
         <div className="container mx-auto gap-8 px-4 py-20 lg:grid lg:grid-cols-2">
-          <figure className="mb-8 flex items-center justify-center bg-neutral-100 py-16 lg:mb-0">
-            <img
-              className="block h-60 w-40 object-cover"
-              src={book.imgUrl}
-              alt={`cover of ${book.title}`}
-            />
-          </figure>
-          <div className="flex flex-col gap-4 lg:justify-between lg:gap-0">
-            <div>
-              <p className="text-2xl font-semibold">{book.title}</p>
-              <p className="text-xl font-medium">{book.author}</p>
-            </div>
-            <div>
-              <p className="text-2xl font-bold text-green-600">
-                {book.unitPrice}
-              </p>
-              <p>
-                Condition: <span className="font-medium">{book.condition}</span>
-              </p>
-            </div>
-            <div>
-              <p>{book.category[0]}</p>
-              <p>
-                {book.format}, {book.pageCount} pages
-              </p>
-            </div>
-            <p className="pb-8">{book.description}</p>
-            <button
-              className="rounded bg-zinc-800 py-3 text-xs font-bold uppercase tracking-wider text-white hover:bg-zinc-950"
-              type="button"
-            >
-              Add to bag
-            </button>
-          </div>
+          {(() => {
+            const {
+              imgUrl,
+              title,
+              author,
+              unitPrice,
+              condition,
+              category,
+              format,
+              pageCount,
+              description,
+            } = book;
+            const dollarsAmount = formatPrice(unitPrice);
+            return (
+              <>
+                <figure className="mb-8 flex items-center justify-center bg-neutral-100 py-16 lg:mb-0">
+                  <img
+                    className="block h-60 w-40 object-cover"
+                    src={imgUrl}
+                    alt={`cover of ${title}`}
+                  />
+                </figure>
+                <div className="flex flex-col gap-4 lg:justify-between lg:gap-0">
+                  <div>
+                    <p className="text-2xl font-semibold">{title}</p>
+                    <p className="text-xl font-medium">{author}</p>
+                  </div>
+                  <div>
+                    <p className="text-2xl font-bold text-green-600">
+                      {dollarsAmount}
+                    </p>
+                    <p>
+                      Condition:{" "}
+                      <span className="font-medium">{condition}</span>
+                    </p>
+                  </div>
+                  <div>
+                    <p>{category[0]}</p>
+                    <p>
+                      {format}, {pageCount} pages
+                    </p>
+                  </div>
+                  <p className="pb-8">{description}</p>
+                  <button
+                    className="rounded bg-zinc-800 py-3 text-xs font-bold uppercase tracking-wider text-white hover:bg-zinc-950"
+                    type="button"
+                    onClick={() => console.log("add to bag")}
+                  >
+                    Add to bag
+                  </button>
+                </div>
+              </>
+            );
+          })()}
         </div>
       ) : (
         "Hi"
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,9 @@
+export async function getFeaturedBooks() {
+  const response = await fetch("http://localhost:8000/books?_start=0&_limit=4");
+  const data = await response.json();
+  return data;
+}
+
 export function formatPrice(unitPrice) {
   const dollarsAmount = new Intl.NumberFormat("en-CA", {
     style: "currency",
