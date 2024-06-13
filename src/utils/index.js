f8752d8d6c2f1b8750f30c80e3c077b71c9536d4
stautuan <stautuan89@gmail.com>
2024-06-13 08:15:23 -0400

    Changed base url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3004";
+const BASE_URL = "https://book-data-chi.vercel.app";
 
 export async function getBooks() {
   const response = await fetch(`${BASE_URL}/books`);
